416009d9715214063fadb961b893dbc7f1944644
Lukas Bauza <lukasbauza.lb@proton.me>
2026-07-18 12:58:11 +0100

    Clean up

--- system/system.scm
+++ system/system.scm
@@ -1,14 +1,3 @@
-;; This is an operating system configuration generated
-;; by the graphical installer.
-;;
-;; Once installation is complete, you can learn and modify
-;; this file to tweak the system configuration, and pass it
-;; to the 'guix system reconfigure' command to effect your
-;; changes.
-
-
-;; Indicate which modules to import to access the variables
-;; used in this configuration.
 (use-modules
   (gnu)
   (gnu packages freedesktop)
@@ -18,7 +7,7 @@
   (nongnu packages linux)
   (nongnu system linux-initrd)
 
-  (hardware pc-hardware))
+  (hardware pc))
 (use-service-modules cups desktop networking ssh xorg)
 
 (operating-system
@@ -30,7 +19,6 @@
   (keyboard-layout (keyboard-layout "gb"))
   (host-name "guix-system")
 
-  ;; The list of user accounts ('root' is implicit).
   (users (cons* (user-account
                   (name "lukas")
                   (comment "Lukas")
@@ -39,9 +27,6 @@
                   (supplementary-groups '("wheel" "netdev" "audio" "video")))
                 %base-user-accounts))
 
-  ;; Packages installed system-wide.  Users can also install packages
-  ;; under their own account: use 'guix search KEYWORD' to search
-  ;; for packages and 'guix install PACKAGE' to install a package.
   (packages (append (list
 		      ;; Niri stuff
 		      niri
@@ -49,27 +34,20 @@
 		      xdg-desktop-portal-gtk
           network-manager-applet
           blueman
-                     ;; Fonts to cover all languages.
                      (specification->package "font-google-noto")
                      (specification->package "font-google-noto-emoji")
                      (specification->package "font-sarasa-gothic")
 		     (specification->package "flatpak"))
                     %base-packages))
 
-  ;; Below is the list of system services.  To search for available
-  ;; services, run 'guix system search KEYWORD' in a terminal.
   (services
    (append (list (service plasma-desktop-service-type)
 		 (service bluetooth-service-type)
 
-       ;; To configure OpenSSH, pass an 'openssh-configuration'
-       ;; record as a second argument to 'service' below.
        (service openssh-service-type)
        (service cups-service-type)
        (set-xorg-configuration
         (xorg-configuration (keyboard-layout keyboard-layout))))
-   ;; This is the default list of services we
-   ;; are appending to.
    (modify-services %desktop-services
 		   (guix-service-type config => (guix-configuration
 						  (inherit config)
